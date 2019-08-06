7eda918657fda8453569242b9619754291aec949
BigDapan <thomas.beal78@gmail.com>
2019-08-06 12:12:45 +0200

    V2 (duree en minutes:secondes, exception QueueId, tri champ bot croissant)

--- main.py
+++ main.py
@@ -9,19 +9,20 @@ listematch=listematch.json()
 
 listeidmatch=[]
 
+
 for i in listematch['matches']:
     listeidmatch.append(i['gameId'])
 
 champduree={}
-fichiertemps = open("Tempsgames.txt", "a")
+fichiertemps = open("Tempsgames.txt", "w")
 
 for matchid in listeidmatch:
     detailsmatch=requests.get("https://euw1.api.riotgames.com/lol/match/v4/matches/" + str(matchid) + "?api_key=" + APIkey)
     detailsmatch=detailsmatch.json()
     duree=detailsmatch['gameDuration']
     participants=detailsmatch['participantIdentities']
-    if len(participants)>1:
-        fichiertemps.write("\n"+str(duree))
+    if len(participants)>1 and detailsmatch['queueId']==840:
+        fichiertemps.write("\n"+str(duree//60)+":"+str(duree%60))
         for num in range(5,10):
             nom=participants[num]['player']['summonerName']
             if nom in champduree:
@@ -31,15 +32,23 @@ for matchid in listeidmatch:
 
 fichiertemps.close()
 
-fichierbots = open("TempsChampBot.txt", "a")
+
+moyennebot={}
 
 for champ in champduree.keys():
     somme=0
     for temps in champduree[champ]:
         somme+=temps
     moyenne=int(somme/len(champduree[champ]))
-    fichierbots.write("\n"+ champ + ";" +str(moyenne))
-            
+    moyennebot[champ]=moyenne
+    
+
+moyennebot=sorted(moyennebot.items(), key=lambda t: t[1])
+
+
+fichierbots = open("TempsChampBot.txt", "w")
+for i in moyennebot:
+    fichierbots.write("\n"+ i[0] + ";" +str(i[1]//60)+":"+str(i[1]%60))
 fichierbots.close()
 
 print("---Script terminé---")
